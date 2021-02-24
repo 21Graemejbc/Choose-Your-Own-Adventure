4013e3934d56aad2c5fc21a4f81f86a06b1e4a16
graecook575 <78805065+graecook575@users.noreply.github.com>
2021-02-24 14:40:33 -0500

    Add project files.

---
+++ ChooseYourOwnAdventure/Form1.Designer.cs
@@ -0,0 +1,166 @@
+﻿
+namespace ChooseYourOwnAdventure
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.storyOutput = new System.Windows.Forms.Label();
+            this.promptOutput = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.mainImage = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.blueOutput = new System.Windows.Forms.Label();
+            this.redOutput = new System.Windows.Forms.Label();
+            this.greenOutput = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.mainImage)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // storyOutput
+            // 
+            this.storyOutput.Location = new System.Drawing.Point(9, 10);
+            this.storyOutput.Name = "storyOutput";
+            this.storyOutput.Size = new System.Drawing.Size(782, 78);
+            this.storyOutput.TabIndex = 1;
+            this.storyOutput.Text = "You wake up with a concussion, it seems you were knocked out during the evacuatio" +
+    "ns and are the last one remaining onboard the Screaming Firehawk";
+            // 
+            // promptOutput
+            // 
+            this.promptOutput.Location = new System.Drawing.Point(9, 412);
+            this.promptOutput.Name = "promptOutput";
+            this.promptOutput.Size = new System.Drawing.Size(775, 65);
+            this.promptOutput.TabIndex = 2;
+            this.promptOutput.Text = "Do you want to attempt to leave the station in a shuttle or try to conduct repair" +
+    "s?";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::ChooseYourOwnAdventure.Properties.Resources.Blue_Button;
+            this.pictureBox1.Location = new System.Drawing.Point(9, 480);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(42, 41);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // mainImage
+            // 
+            this.mainImage.Location = new System.Drawing.Point(9, 91);
+            this.mainImage.Name = "mainImage";
+            this.mainImage.Size = new System.Drawing.Size(783, 314);
+            this.mainImage.TabIndex = 0;
+            this.mainImage.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::ChooseYourOwnAdventure.Properties.Resources.Green_Button;
+            this.pictureBox2.Location = new System.Drawing.Point(9, 574);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(42, 41);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 4;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = global::ChooseYourOwnAdventure.Properties.Resources.Red_Button;
+            this.pictureBox3.Location = new System.Drawing.Point(9, 527);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(42, 41);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox3.TabIndex = 5;
+            this.pictureBox3.TabStop = false;
+            // 
+            // blueOutput
+            // 
+            this.blueOutput.Location = new System.Drawing.Point(57, 480);
+            this.blueOutput.Name = "blueOutput";
+            this.blueOutput.Size = new System.Drawing.Size(735, 41);
+            this.blueOutput.TabIndex = 6;
+            this.blueOutput.Text = "Repair";
+            // 
+            // redOutput
+            // 
+            this.redOutput.Location = new System.Drawing.Point(57, 527);
+            this.redOutput.Name = "redOutput";
+            this.redOutput.Size = new System.Drawing.Size(735, 41);
+            this.redOutput.TabIndex = 7;
+            this.redOutput.Text = "Escape";
+            // 
+            // greenOutput
+            // 
+            this.greenOutput.Location = new System.Drawing.Point(57, 574);
+            this.greenOutput.Name = "greenOutput";
+            this.greenOutput.Size = new System.Drawing.Size(735, 41);
+            this.greenOutput.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 622);
+            this.Controls.Add(this.greenOutput);
+            this.Controls.Add(this.redOutput);
+            this.Controls.Add(this.blueOutput);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.promptOutput);
+            this.Controls.Add(this.storyOutput);
+            this.Controls.Add(this.mainImage);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "The Screaming Firehawk";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.mainImage)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox mainImage;
+        private System.Windows.Forms.Label storyOutput;
+        private System.Windows.Forms.Label promptOutput;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.Label blueOutput;
+        private System.Windows.Forms.Label redOutput;
+        private System.Windows.Forms.Label greenOutput;
+    }
+}
+
---
+++ ChooseYourOwnAdventure/Form1.cs
@@ -0,0 +1,355 @@
+﻿/* Graeme Cook
+ * February 24, 2021
+ * A game about escaping/repairing a spaceship*/
+
+//Could do specific death messages, but unsure
+
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Media;
+
+namespace ChooseYourOwnAdventure
+{
+    public partial class Form1 : Form
+    {
+        int scene = 0;
+        int dead = 97;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.M) //Red button
+            {
+                if (scene == 0)  
+                {
+                    Random randGen = new Random();
+                    int chance = randGen.Next (1, 11);
+
+                    if (chance <= 1) { scene = 96; }
+                    else { scene = dead; }
+                }
+
+                else if (scene == 3)  { scene = 5; }
+                else if (scene == 4)  { }
+                else if (scene == 5)  { scene = dead; }
+                else if (scene == 6)  { scene = dead; }
+                else if (scene == 7)  { }
+                else if (scene == 8)  { scene = dead; }
+                else if (scene == 9)  { }
+                else if (scene == 10) { }
+                else if (scene == 11) { }
+                else if (scene == 12) { }
+                else if (scene == 13) { scene = 15; }
+                else if (scene == 14) { }
+                else if (scene == 15) { scene = 23; }
+                else if (scene == 16) { }
+                else if (scene == 17) { }
+                else if (scene == 18) { }
+                else if (scene == 19) { }
+                else if (scene == 20) { }
+                else if (scene == 21) { }
+                else if (scene == 22) { }
+                else if (scene == 23) { }
+                else if (scene == 24) { }
+                else if (scene == 25) { }
+                else if (scene == 26) { }
+                else if (scene == 27) { }
+                else if (scene == 28) { }
+                else if (scene == 96) { scene = 0; }
+                else if (scene == 97) { scene = 0; }
+                else if (scene == 98) { scene = 0; }
+                else if (scene == 99) { scene = 0; }
+            }
+
+            else if (e.KeyCode == Keys.B) //Blue button
+            {
+                if (scene == 0)       { scene = 3; }
+                else if (scene == 3)  { scene = dead; }
+                else if (scene == 4)  { }
+                else if (scene == 5)  { scene = 6; }
+                else if (scene == 6)  { scene = 8; }
+                else if (scene == 7)  { }
+                else if (scene == 8)  { scene = 13; }
+                else if (scene == 9)  { }
+                else if (scene == 10) { }
+                else if (scene == 11) { }
+                else if (scene == 12) { }
+                else if (scene == 13) { scene = 14; }
+                else if (scene == 14) { }
+                else if (scene == 15) { scene = 20; }
+                else if (scene == 16) { }
+                else if (scene == 17) { }
+                else if (scene == 18) { }
+                else if (scene == 19) { }
+                else if (scene == 20) { }
+                else if (scene == 21) { }
+                else if (scene == 22) { }
+                else if (scene == 23) { }
+                else if (scene == 24) { }
+                else if (scene == 25) { }
+                else if (scene == 26) { }
+                else if (scene == 27) { }
+                else if (scene == 28) { }
+                else if (scene == 96) { Application.Exit(); }
+                else if (scene == 97) { Application.Exit(); }
+                else if (scene == 98) { Application.Exit(); }
+                else if (scene == 99) { Application.Exit(); }
+            }
+
+            else if (e.KeyCode == Keys.Space) //Green button
+            {
+                if (scene == 0)       { }
+                else if (scene == 3)  { scene = 6; }
+                else if (scene == 4)  { }
+                else if (scene == 5)  { }
+                else if (scene == 6)  { }
+                else if (scene == 7)  { }
+                else if (scene == 8)  
+                { 
+                    scene = 10;
+
+                    Random randGen = new Random();
+                    int chance = randGen.Next(1, 11);
+
+                    if (chance <= 4) { scene = 96; }
+                    else { scene = dead; }
+                }
+
+                else if (scene == 9)  { }
+                else if (scene == 10) { }
+                else if (scene == 11) { }
+                else if (scene == 12) { }
+                else if (scene == 13) { }
+                else if (scene == 14) { }
+                else if (scene == 15) { }
+                else if (scene == 16) { }
+                else if (scene == 17) { }
+                else if (scene == 18) { }
+                else if (scene == 19) { }
+                else if (scene == 20) { }
+                else if (scene == 21) { }
+                else if (scene == 22) { }
+                else if (scene == 23) { }
+                else if (scene == 24) { }
+                else if (scene == 25) { }
+                else if (scene == 26) { }
+                else if (scene == 27) { }
+                else if (scene == 28) { }
+                else if (scene == 96) { }
+                else if (scene == 97) { }
+                else if (scene == 98) { }
+                else if (scene == 99) { }
+            }
+
+            switch (scene)
+            {
+                case 0:
+                    storyOutput.Text = "You wake up with a concussion, it seems you were knocked out during the evacuations and are the last one remaining onboard the Screaming Firehawk";
+                    promptOutput.Text = "Do you want to attempt to leave the station in a shuttle? Or, try to conduct repairs?";
+
+                    redOutput.Text = "Escape";
+                    blueOutput.Text = "Repair";
+                    greenOutput.Text = "";
+                    break;
+
+                case 3:
+                    storyOutput.Text = "You come to a crossroads in engineering; the PA blares about a reactor issue, but the air is staring to feel thin as well...";
+                    promptOutput.Text = "Will you go to reactor control, life support, or the power relay station first?";
+
+                    redOutput.Text = "Life Support";
+                    blueOutput.Text = "Power";
+                    greenOutput.Text = "Reactor Control";
+                    break;
+
+                case 4:
+                    //Output text
+                    //Option texts
+                    break;
+
+                case 5:
+                    storyOutput.Text = "You arrive at the life support station. The interfaces are dark. The ongoing power issues must be affecting the core systems.";
+                    promptOutput.Text = "Move on to reactor control or the power relay?";
+
+                    redOutput.Text = "Power";
+                    blueOutput.Text = "Reactor Control";
+                    greenOutput.Text = "";
+                    break;
+
+                case 6:
+                    storyOutput.Text = "In reactor control, the panels are flashing erratically. What about? You don't know. One screen flashes 'core dump required' and a big red button prompts you.";
+                    promptOutput.Text = "Do you want to push the button?";
+
+                    redOutput.Text = "No";
+                    blueOutput.Text = "Yes";
+                    greenOutput.Text = "";
+                    break;
+
+                case 7:
+                    //Output text
+                    //Option texts
+                    break;
+
+                case 8:
+                    storyOutput.Text = "There's a blast of bright light as a ball of super-heated plasma is ejected out behind the station; it seems you made a good choice coming here first. As you catch your breathe you realize the air is getting thinner...";
+                    promptOutput.Text = "Next, do you want to try to repair the life support systems, repair the power, or double back to the shuttle?";
+
+                    redOutput.Text = "Power";
+                    blueOutput.Text = "Life Support";
+                    greenOutput.Text = "Go back to the shuttle";
+                    break;
+
+                case 9:
+                    //Output text
+                    //Option texts
+                    break;
+
+                case 10:
+                    storyOutput.Text = "On your way back to the shuttle, you pass the fuel depot and grab some fuel for the shuttle... that should help.";
+                    promptOutput.Text = "";
+
+                    redOutput.Text = "";
+                    blueOutput.Text = "";
+                    greenOutput.Text = "";
+
+                    break;
+
+                case 11:
+                    //Output text
+                    //Option texts
+                    break;
+                case 12:
+                    //Output text
+                    //Option texts
+                    break;
+
+                case 13:
+                    storyOutput.Text = "In life support, you notice a circuit overload caused the air recyclers to shut down, a manual reset fixes things.";
+                    promptOutput.Text = "Do you head to power, to hopefully complete the repairs or do you begin to investigate what happened to the ship?";
+
+                    redOutput.Text = "Investigate";
+                    blueOutput.Text = "Go to power";
+                    greenOutput.Text = "";
+                    break;
+
+                case 14:
+                    storyOutput.Text = "The power grid seems to have managed to automatically stabilized. Before you turn to leave, you notice a blinking light in the corner.";
+                    promptOutput.Text = "Investigate the light or move on?";
+
+                    redOutput.Text = "Investigate";
+                    blueOutput.Text = "Move on"; //You are here
+                    greenOutput.Text = "";
+                    break;
+
+                case 15:
+                    storyOutput.Text = "The emergency clearly stemmed from the ship's reactor. You could dig around engineering or the flight deck for clues...";
+                    promptOutput.Text = "Head back to engineering or go up to the flight deck?";
+
+                    redOutput.Text = "Go to the flight deck";
+                    blueOutput.Text = "Back to engineering";
+                    greenOutput.Text = "";
+                    break;
+
+                case 16:
+                    //Output text
+                    //Option texts
+                    break;
+                case 17:
+                    //Output text
+                    //Option texts
+                    break;
+                case 18:
+                    //Output text
+                    //Option texts
+                    break;
+                case 19:
+                    //Output text
+                    //Option texts
+                    break;
+                case 20:
+                    //Output text
+                    //Option texts
+                    break;
+                case 21:
+                    //Output text
+                    //Option texts
+                    break;
+                case 22:
+                    //Output text
+                    //Option texts
+                    break;
+                case 23:
+                    //Output text
+                    //Option texts
+                    break;
+                case 24:
+                    //Output text
+                    //Option texts
+                    break;
+                case 25:
+                    //Output text
+                    //Option texts
+                    break;
+                case 26:
+                    //Output text
+                    //Option texts
+                    break;
+                case 27:
+                    //Output text
+                    //Option texts
+                    break;
+                case 28:
+                    //Output text
+                    //Option texts
+                    break;
+
+                case 96:
+                    storyOutput.Text = "You made it out on the shuttle! Congratulations on escaping the Screaming Firehawk!";
+                    promptOutput.Text = "Would you like to play again?";
+
+                    redOutput.Text = "Yes";
+                    blueOutput.Text = "No";
+                    greenOutput.Text = "";
+                    break;
+
+                case 97:
+                    storyOutput.Text = "You're now just another number in the death toll of the Screaming Firehawk Incident.";
+                    promptOutput.Text = "Would you like to play again?";
+
+                    redOutput.Text = "Yes";
+                    blueOutput.Text = "No";
+                    greenOutput.Text = "";
+                    break;
+
+                case 98:
+                    storyOutput.Text = "Congratulations! You found the secret ending and turned the saboteur's plan against him!";
+                    promptOutput.Text = "Would you like to play again?";
+
+                    redOutput.Text = "Yes";
+                    blueOutput.Text = "No";
+                    greenOutput.Text = "";
+                    break;
+
+                case 99:
+                    storyOutput.Text = "Congratulations! You completed the main story and retook the ship, try discovering the secret ending!";
+                    promptOutput.Text = "Would you like to play again?";
+
+                    redOutput.Text = "Yes";
+                    blueOutput.Text = "No";
+                    greenOutput.Text = "";
+                    break;
+            }
+        }
+    }
+}
